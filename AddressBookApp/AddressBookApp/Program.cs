1b71f29419bf8486af8638c41d6f5b1e0c2e5eb0
nakul465 <nakularora465@gmail.com>
2026-09-14 00:06:52 +0530

    Implement UC1 : Create Contact class with a method ToString

--- AddressBookApp/AddressBookApp/Program.cs
+++ AddressBookApp/AddressBookApp/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using AddressBookApp;
+using AddressBookApp.Models;
+
+Contact person1 = new Contact("John","Doe", "12 MG Road","Pune", "Maharashtra", "411001", "9876543210", "john.doe@mail.com");
+Console.WriteLine(person1.ToString());
