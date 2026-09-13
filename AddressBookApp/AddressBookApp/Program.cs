a1e866b48b15a67844813c198fb36774acfc12b8
nakul465 <nakularora465@gmail.com>
2026-09-14 02:04:45 +0530

    Implemented UC3: Address Book + Add Contacts

--- AddressBookApp/AddressBookApp/Program.cs
+++ AddressBookApp/AddressBookApp/Program.cs
@@ -1,7 +1,47 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using AddressBookApp;
 using AddressBookApp.Models;
+using AddressBookApp.Services;
 
-Contact person1 = new Contact("John","Doe", "12 MG Road","Pune", "Maharashtra", "411001", "9876543210", "john.doe@mail.com");
+AddressBook book = new AddressBook();
 
-Console.WriteLine(person1.ToString());
+while (true)
+{
+    Console.WriteLine();
+    Console.WriteLine("--------------Menu--------------");
+    Console.WriteLine();
+    Console.WriteLine("1. Add Contact\n2. Show All Contact\n0. Exit");
+    Console.WriteLine();
+
+    int input = Convert.ToInt32(Console.ReadLine());
+    if (input == 1)
+    {
+        Console.Write("Enter First Name: ");
+        string? firstName = Console.ReadLine();
+        Console.Write("Enter Last Name: ");
+        string? lastName = Console.ReadLine();
+        Console.Write("Enter Address: ");
+        string? address = Console.ReadLine();
+        Console.Write("Enter City Name: ");
+        string? city = Console.ReadLine();
+        Console.Write("Enter State Name: ");
+        string? state = Console.ReadLine();
+        Console.Write("Enter ZIP Code: ");
+        string? zip = Console.ReadLine();
+        Console.Write("Enter Phone Number: ");
+        string? phoneNumber = Console.ReadLine();
+        Console.Write("Enter Email Address: ");
+        string? email = Console.ReadLine();
+
+        Contact c = new Contact(firstName!, lastName!, address!, city!, state!, zip!, phoneNumber!, email!);
+        book.AddContact(c);
+    }
+    else if (input == 2)
+    {
+        book.PrintAll();
+    }
+    else if (input == 0)
+    {
+        break;
+    }
+
+}
---
+++ AddressBookApp/AddressBookApp/Services/AddressBook.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using AddressBookApp.Models;
+namespace AddressBookApp.Services
+{
+	public class AddressBook
+	{
+		private List<Contact> contacts;
+
+		public AddressBook()
+		{
+			contacts = new();
+		}
+
+		public void AddContact(Contact c)
+		{
+			contacts.Add(c);
+			Console.WriteLine("Contact Added Successfully");
+		}
+
+		public void PrintAll()
+		{
+			foreach(Contact c in contacts)
+			{
+				Console.WriteLine(c.ToString());
+			}
+        }
+	}
+}
+
